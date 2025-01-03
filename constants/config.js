c04baa4572fd019eacb7270c49f9eed4a3fd68d5
Palash-Tinkhede <palash.tinkhede@gmail.com>
2025-01-03 23:10:54 +0530

    corsOption frontend url added

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,6 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
+   "https://chat-box-client-iota.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
